6ee2f7f1b10b0b26e87dcc595b1fa96039f7be8e
André Rosatto <linus1178@gmail.com>
2025-02-26 20:47:43 -0300

    minor adjustments

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 export default function useFirebase() {
-	const firebaseConfig = {
+	const app = initializeApp({
 		apiKey: import.meta.env.VITE_API_KEY,
 		authDomain: import.meta.env.VITE_AUTH_DOMAIN,
 		projectId: import.meta.env.VITE_PROJECT_ID,
 		storageBucket: import.meta.env.VITE_STORAGE_BUCKER,
 		messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
 		appId: import.meta.env.VITE_APP_ID,
-	};
-	const app = initializeApp(firebaseConfig);
+	});
 	const db = getFirestore(app);
 
 	return db;
