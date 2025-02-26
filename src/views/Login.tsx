ba5d4fc8751a2a10da86579b6006d814c17e3e50
André Rosatto <linus1178@gmail.com>
2025-02-26 20:03:16 -0300

    auth

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import useFirebase from "../hooks/useFirebase";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import '../css/LoginView.css';
@@ -10,6 +10,7 @@ interface LoginViewProps {
 export default function LoginView({ onLogin }: LoginViewProps) {
 	const [showError, setShowError] = useState(false);
 	const [password, setPassword] = useState('');
+	const inputRef = useRef<HTMLInputElement>(null);
 	const db = useFirebase();
 
 	const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,34 +18,35 @@ export default function LoginView({ onLogin }: LoginViewProps) {
 		setPassword(e.currentTarget.value);
 	}
 
-	const handleClick = async () => {
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 		const q = query(collection(db, 'users'), where('password', '==', password));
 		const docs = await getDocs(q);
 		if (docs.size > 0) {
 			onLogin();
 		} else {
 			setShowError(true);
+			setPassword('');
+			inputRef.current?.focus();
 		}
 	}
 
 	return (
 		<main className='LoginView'>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<h2>Login</h2>
 				<label>
 					Senha:
 					<input
+						ref={inputRef}
+						autoFocus
 						type='password'
 						value={password}
 						onChange={handlePasswordChange}
 					/>
 				</label>
 				{showError && <p>Senha incorreta</p>}
-				<button
-					disabled={password.length === 0}
-					type='button'
-					onClick={handleClick}
-				>Entrar</button>
+				<button disabled={password.length === 0}>Entrar</button>
 			</form>
 		</main>
 	)
